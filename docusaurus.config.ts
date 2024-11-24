84341e06b90ea6661cc165f18556b64ac00325d1
lazydukk <banul07@proton.me>
2024-11-25 00:42:12 +0530

    docu conf

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,7 +22,6 @@ const config: Config = {
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  trailingSlash: 'true',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
